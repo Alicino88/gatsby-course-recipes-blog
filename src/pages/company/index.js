c54ee55e93263611900b0a31bff521a6e28c910c
Alice <alice.moretti@truemedinc.com>
2021-08-13 10:03:43 +0300

    Created index file and added comments

---
+++ src/pages/company/index.js
@@ -0,0 +1,8 @@
+//in this case index.js leads to http://localhost:8000/company
+import React from "react"
+
+function company() {
+  return <div>this is the main company page</div>
+}
+
+export default company
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
+//Index is the hp
+/*the hp, index.js, like the other pages, are wrapped around the Layout component
+The Layout component contains the elements that are always visible, such as the navbar, and the actual
+content of the page (in this case the h1Content of the homepageH1) that is passed using children prop
+to the Layout component*/
+
 import React from "react"
 
+import Layout from "../components/Layout.js"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Layout>
+        {/*style: the first bracket is to access javascript and the second to access the object */}
+        <h1 style={{ color: "blue", textTransform: "uppercase" }}>
+          Content of the homepage
+        </h1>
+      </Layout>
+    </div>
+  )
 }
