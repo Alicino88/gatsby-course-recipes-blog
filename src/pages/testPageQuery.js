61891378341d3f41c3c20e0d1e0bb8d394ac18e6
Alice <alice.moretti@truemedinc.com>
2021-08-17 11:31:44 +0300

    Added comments to testPageQuery.js

--- src/pages/testPageQuery.js
+++ src/pages/testPageQuery.js
@@ -1,8 +1,9 @@
-//Example of page Query
+//Example of Page Query
 import React from "react"
 import { graphql } from "gatsby"
-//when we export our query as a constant and our component has props,
-//the data retrieved from the query are listed automatically inside the props
+/*when we export our query as a constant and our component has props,
+the data retrieved from the query are listed automatically inside the props
+and thus we can access them*/
 export const data = graphql`
   {
     site {
@@ -23,11 +24,25 @@ export const data = graphql`
     }
   }
 `
-const testPageQuery = props => {
-  console.log(props)
+/*
+const testPageQuery = (props) => {
+  console.log(data)
   return (
     <div>
       <h2>testing {props.data.site.siteMetadata.author}</h2>
+    </div>
+  )
+}*/
+
+//this is the same as above using prop
+//we can destructure the prop and access the data:
+const testPageQuery = ({ data }) => {
+  console.log(data)
+  //we can define a variable so that is easier to reuse across the html
+  const author = data.site.siteMetadata.author
+  return (
+    <div>
+      <h2>testing {author}</h2>
     </div>
   )
 }
