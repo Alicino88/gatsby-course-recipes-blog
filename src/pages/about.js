afe3de980b2d6e26f1e858b3d38a3b0b4f010ba2
Alice <alice.moretti@truemedinc.com>
2021-08-17 08:56:26 +0300

    updates to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,37 @@
 import React from "react"
 import Layout from "../components/Layout.js"
-
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 function About() {
   return (
     <Layout>
-      <h1>About page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I am the title of the page</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </p>
+            <p>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum."
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person pouring salt in bowl"
+            placeholder="tracedSVG"
+            className="about-img"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
