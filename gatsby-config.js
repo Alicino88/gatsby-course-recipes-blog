481bfce17ad398e036b3d198c8496c7f55e24cff
Alice <alice.moretti@truemedinc.com>
2021-08-17 11:14:03 +0300

    added siteMetadata to gatsby confing

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@
 
 module.exports = {
   /* Your site config here */
+  /*below metadata to practice GraphQL */
+  siteMetadata: {
+    title: "Simply recipes",
+    description: "Nice and clean recipes site",
+    author: "Alice Moretti",
+    person: { name: "John", age: 33 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "John", age: 33 },
+      { name: "Susan", age: 35 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
