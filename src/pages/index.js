56ebbd942264684d431e26474fb7dba5ae53d035
Alice <alice.moretti@truemedinc.com>
2021-08-17 08:56:51 +0300

    Updates to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,37 @@
 The Layout component contains the elements that are always visible, such as the navbar, and the actual
 content of the page (in this case the h1Content of the homepageH1) that is passed using children prop
 to the Layout component*/
+/*example of inline styling:
+ <Layout>
+        {style: the first bracket is to access javascript and the second to access the object }
+        <h1 style={{ color: "blue", textTransform: "uppercase" }}></h1>
 
+*/
 import React from "react"
 
 import Layout from "../components/Layout.js"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
-    <div>
-      <Layout>
-        {/*style: the first bracket is to access javascript and the second to access the object */}
-        <h1 style={{ color: "blue", textTransform: "uppercase" }}>
-          Content of the homepage
-        </h1>
-      </Layout>
-    </div>
+    <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Simply recipes</h1>
+              <h4>Delicious recipes for everiday life</h4>
+            </div>
+          </div>
+        </header>
+      </main>
+    </Layout>
   )
 }
