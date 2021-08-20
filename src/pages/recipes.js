2b1479f053d78d73881866a9b323b9b70f052219
Alice <alice.moretti@truemedinc.com>
2021-08-20 12:55:50 +0300

    Added AllRecipes component to recipes page

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout.js"
+import AllRecipes from "../components/AllRecipes.js"
 
 function Recipes() {
   return (
     <Layout>
-      <h1>Recipe page</h1>
+      <main className="page">
+        <AllRecipes />
+      </main>
     </Layout>
   )
 }
