e69229b05931b79e6cc674794f96a53eed1b89fc
Alice <alice.moretti@truemedinc.com>
2021-08-25 10:42:42 +0300

    Added programmatic pages using file system route API

---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,135 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs"
+import Layout from "../components/Layout.js"
+import SEO from "../components/SEO.js"
+/*when I click on the recipe on the RecipesList.js component the title of the recipe is "slugified"
+the dynamic page that opens up {ContentfulRecipe.title}.js uses the query variable $title to get
+the data from the specific recipe. Gatsby automatically creates a page for each recipe when we create the file
+{ContentfulRecipe.title}.js inside the pages folder.
+*/
+const RecipeTemplate = ({ data }) => {
+  console.log(data)
+  const {
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image,
+  } = data.contentfulRecipe
+  const pathToImage = getImage(image)
+  const { tags, instructions, ingredients, tools } = content
+  return (
+    <Layout>
+      <SEO title={title} description={description} />
+      <main className="page">
+        <div className="recipe-page">
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-img"
+            ></GatsbyImage>
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              <p className="recipe-tags">
+                Tags :
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/$tag`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+          <section className="recipe-content">
+            <article>
+              <h4>Instructions</h4>
+              {instructions.map((item, index) => {
+                return (
+                  <div key={index} className="single-instruction">
+                    <header>
+                      <p>step {index + 1}</p>
+                      <div></div>
+                    </header>
+                    <p>{item}</p>
+                  </div>
+                )
+              })}
+            </article>
+            <article className="second-column">
+              <div>
+                <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return (
+                    <p key={index} className="single-ingredient">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+              <div>
+                <h4>tools</h4>
+                {tools.map((item, index) => {
+                  return (
+                    <p key={index} className="tool">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </Layout>
+  )
+}
+
+//$title is a query variable
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
+export default RecipeTemplate
