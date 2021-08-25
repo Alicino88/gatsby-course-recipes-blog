e9c272e67a532b11b768821128fddd69ab9d0ec1
Alice <alice.moretti@truemedinc.com>
2021-08-25 10:41:17 +0300

    Added programmatic pages using Gatsby Node API

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,18 +1,22 @@
 import React from "react"
 import Layout from "../components/Layout.js"
 import setupTags from "../utils/setupTags"
+import slugify from "slugify"
 import { graphql, Link } from "gatsby"
+import SEO from "../components/SEO.js"
 
 function Tags({ data }) {
   const newTags = setupTags(data.allContentfulRecipe.nodes)
   return (
     <Layout>
+      <SEO title="Tags" />
       <main className="page">
         <section className="tags-page">
           {newTags.map((tag, index) => {
             const [text, value] = tag
+            const slug = slugify(text, { lower: true })
             return (
-              <Link to={`/${text}`} key={index} className="tag">
+              <Link to={`/tags/${slug}`} key={index} className="tag">
                 <h5>{text}</h5>
                 <p>{value} recipe</p>
               </Link>
